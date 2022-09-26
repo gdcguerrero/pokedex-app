084f9901f946bc521241655b3ef5b802a3d22ac6
Narbok <narbok29@gmail.com>
2022-09-26 11:38:41 -0500

    ejemplos

--- src/app/components/models/auth/auth-routing.module.ts
+++ src/app/components/models/auth/auth-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
         component: LoginComponent
       },
       {
-        path: 'enroll',
+        path: 'enroll/:id',
         component: EnrollComponent
       },
       {
